028755eb8a8d654a1cea533fe96d414a20eb033c
Cedricst789 <1937033@cmontmorency.qc.ca>
2025-12-15 13:37:52 -0500

    v2_push

--- tp4_cedric_sirois-tanguay/scenes/main_2.gd
+++ tp4_cedric_sirois-tanguay/scenes/main_2.gd
@@ -17,4 +17,4 @@ func _on_chrono_timeout() -> void:
 
 func _on_drapeau_2_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
 	if body.is_in_group("player"):
-		get_tree().change_scene_to_file("res://scenes/menufin.tscn")
+		get_tree().change_scene_to_file("res://scenes/main_3.tscn")
